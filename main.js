b55180679c9b79ab4f42824461601b7ef69fad4a
deviant <mb.deviant@gmail.com>
2023-02-18 02:09:11 +0300

    convert function to async/await

--- main.js
+++ main.js
@@ -16,16 +16,13 @@ button.addEventListener("click", () => {
   fetchData();
 });
 
-function fetchData(keyword = "cats") {
-  fetch(
+async function fetchData(keyword = "cats") {
+  const response = await fetch(
     `https://api.giphy.com/v1/gifs/translate?api_key=${SECRET_KEY}&s=${keyword}`,
     { mode: "cors" }
-  )
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (response) {
-      const imgData = response.data.images.original.url;
-      img.src = imgData;
-    });
+  );
+  response.json().then(function (response) {
+    const imgData = response.data.images.original.url;
+    img.src = imgData;
+  });
 }
